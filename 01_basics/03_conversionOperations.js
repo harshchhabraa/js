70f32bc3fde5f33b2ca2ac0db022b9c446a64e96
harshchhabraa <harshchhabra0512@gmail.com>
2025-01-30 08:13:21 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+/*let score = "3abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+
+//"33" => 33
+// "33abc" => NaN   not a number
+// true => 1 and vice versa with false
+
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => ture; 0 => false
+// "" => false
+// "harsh" =>true
