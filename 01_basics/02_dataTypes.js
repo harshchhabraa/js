9d8d142b0ad1072a7c58ea3f8ae7981147a759d8
harshchhabraa <harshchhabra0512@gmail.com>
2025-01-30 08:00:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all the code as newer version of js   
+console.log(3+3);
+console.log("Harsh");
+
+let name = 'harsh'
+let age = 19
+let isLoggedIn = false
+
+
+// number => 2 to the power of 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+//object 
+
+console.log(typeof age);
+
+
