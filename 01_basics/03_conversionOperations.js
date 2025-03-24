83c90ed4a02dd560ff5beb466bac2f483ea63c87
harshchhabraa <harshchhabra0512@gmail.com>
2025-03-24 18:38:12 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//conversions:
+//"33" -> 33
+//"33abc" -> NaN Not a number
+//true -> 1
+//false -> 0
+
+let isLoggedIn = "harsh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//Conversions:
+//1 -> true; 0 -> false
+//"" -> false
+//"harsh" -> true 
+ 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+
