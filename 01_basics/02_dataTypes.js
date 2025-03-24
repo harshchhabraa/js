690b66be39b8cf6e4f154edfc35716f1f6832ad7
harshchhabraa <harshchhabra0512@gmail.com>
2025-03-24 16:46:03 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";   //used to treat all js code as newer version
+
+//alert(3+3)// can't be used in node, only works in browser
+
+let name = "harsh"
+let age = 3
+let isLoggedIn = false
+
+//number  -> 2 to the power of 53
+//bigint
+//string -> ""
+//boolean -> true or false
+//null -> standalone value
+//undefined -> 
+//symbol -> unique
+
+
+//object
+console.log(undefined);
+console.log(typeof null);
