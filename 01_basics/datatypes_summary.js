c9f01d93cb420080d14a1c398df1eb8735d0abc0
harshchhabraa <harshchhabra0512@gmail.com>
2025-03-29 10:59:47 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+//Primitive dataypes (call by value):
+//7 types: string , number , boolean , null , undefined , symbol , bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+//Non primitive dataypes (call by reference):
+//arrays , objects , functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
