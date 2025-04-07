eee09bad91ac3fcc5b2a012bda949c47066a9de2
harshchhabraa <harshchhabra0512@gmail.com>
2025-04-07 18:17:55 +0000

    dates in JS

---
+++ 01_basics/07_datesin_JS.js
@@ -0,0 +1,23 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023 , 0 , 23);        //month starts from 0 in JS instead of 1
+let myCreatedDate = new Date(2023 , 2 , 41);        
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+
+
+
+
+
+
+
