f3e74e962560215d374204910073864ca7ad20ee
harshchhabraa <harshchhabra0512@gmail.com>
2025-04-09 17:35:59 +0000

    arrays part 2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -17,6 +17,17 @@ const real_another_array = another_array.flat(Infinity);
 
 
 
-console.log(Array.from("harsh"));
+// console.log(Array.from("harsh"));
+
+// console.log(Array.from({name: "harsh"}));       interesting case: results in an empty array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1 , score2 , score3));
+
+
+
 
 
