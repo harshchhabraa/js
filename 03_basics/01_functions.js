54d5945c39ab2fb5f25da2d76682212235db5001
harshchhabraa <harshchhabra0512@gmail.com>
2025-07-05 13:30:57 +0000

    functions objects and arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,4 +24,31 @@ function LoginUserMessage(username) {
     return `${username} just logged in`;
 }
 // console.log(LoginUserMessage("Harsh"));
-console.log(LoginUserMessage());
+// console.log(LoginUserMessage());
+
+function calculateCartPrice(val1 , val2 , ...num1) {
+    return num1;
+}
+// console.log(calculateCartPrice(200 , 500 , 700 , 2000));
+
+const user = {
+    username : "harsh" ,
+    price : 5000
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+// handleObject(user);
+// // handleObject({
+//     username : "sam",
+//     price : 200
+// })
+
+
+const myNewArray = [200 , 400 , 600 , 800];
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+console.log(returnSecondValue(myNewArray));
